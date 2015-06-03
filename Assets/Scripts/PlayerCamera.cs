07380e1f4a6921995451a4cb3930b3ed77d44501
a virtual fox <anton.alenov12@imperial.ac.uk>
2015-06-03 19:55:06 +0100

    Changed the camera zoom to be more responsive, smaller and more efficent. It is way cooler. At least 20% so. Also contains code for zooming the camera out when it is close to the border, but that code is terrible.

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -13,7 +13,6 @@ public class PlayerCamera : MonoBehaviour {
 		set
 		{
 			player = value;
-			origin = value.position;
 			CameraTransform.position = (Vector2)value.position;
 		}
 	}
@@ -21,45 +20,28 @@ public class PlayerCamera : MonoBehaviour {
 	Transform CameraTransform {get; set;}
 	/* Camera component accessor.  */
 	Camera Camera {get; set;}
+
 	/* Zoom control variables. */
 	public float zoomMod = 2;
 	public float minZoom = .3f;
 	public float maxZoom = 1.5f;
-	public float defaultSize = 1;
-
-	Vector2 maxOffsetVector;
-	Vector2 previousOffsetVector;
-	Vector2 origin;
-	Vector2 target;
-
+	public float defaultZoom = 1;
+	public float singleZoomTime = 0.5f; // time it takes to scroll over a single click of the scroll wheel
 	float previousZoomValue;
 	float nextZoomValue;
 	float zoomLerp = 0;
-	public float totalZoomLerp = 0.35f;
+	float inverseTotalZoomTime;
 
-	/* Unused camera lerp variables. */
-	bool targetIsMoving = false;
-	float lerpTime = 0;
-
-	float lerp; // used to store temporary lerp values in LateUpdate
-
-
-	public float stopTime = 0.1f;
-	public float totalLerpTime = 1.5f;
-	public float maxCameraOffset = 2;
-	public float weightToMouse = 0.5f;
+	/* Smart camera movement. */
+	public float edgeHotspot = 0.85f;
 
 	/* Early bird catches the early worm. */
 	void Awake ()
 	{
 		Camera = GetComponent<Camera> ();
 		Camera.orthographic = true;
-		Camera.orthographicSize = previousZoomValue = nextZoomValue = defaultSize;
+		Camera.orthographicSize = previousZoomValue = nextZoomValue = defaultZoom;
 		CameraTransform = GetComponent<Transform> ();
-
-
-		maxOffsetVector = Vector2.zero;
-		previousOffsetVector = Vector2.zero;
 	}
 
 	/* Get camera to follow object. */
@@ -67,65 +49,46 @@ public class PlayerCamera : MonoBehaviour {
 	{
 		if (Player)
 		{
-			Vector2 mouseOffset = Camera.ScreenToWorldPoint (InputManager.Instance.MousePosition);
-
-
-
-			origin = target = (Vector2)Player.position + weightToMouse * mouseOffset;
-
-			// if (InputManager.Instance.IsMoving ())
-			// {
-			// 	//update input only if input is still happening
-			// 	maxOffsetVector = InputManager.Instance.GetNormalizedInputVector() * maxCameraOffset;
-			// 	if (false && previousOffsetVector != maxOffsetVector)
-			// 	{
-			// 		previousOffsetVector = maxOffsetVector;
-			// 		lerpTime = 0;
-			// 	}
-			// 	if (lerpTime < totalLerpTime)
-			// 	{
-			// 		lerpTime += Time.deltaTime;
-			// 	}
-			// }
-			// else
-			// {
-			// 	//origin = Player.position;
-			// 	if (lerpTime > 0)
-			// 	{
-			// 		lerpTime -= Time.deltaTime;
-			// 	}
-
-			// }
-
-			// setup lerp targets/origins
-			//Vector2 target = (Vector2)Player.position - maxOffsetVector;
-
-			// silky-smooth transition here
-			lerp = LerpFunctions.SmoothStep (lerpTime/totalLerpTime);
-			Vector3 pos = Vector2.Lerp (origin, target, lerp);
-
+			/* Moving camera with player. */
+			Vector3 pos = (Vector2)Player.position;
 			// Set new camera position (conserves current z)
 			pos.z = CameraTransform.position.z;
 			CameraTransform.position = pos;
 
 
-			/* Zoom controls. */
+			/* Zoom out as mouse pointer leaves the screen. (unimplemented) */
+			// offset between mouse pointer and player world coordinates
+			// Vector2 mouseOffset = Camera.ScreenToWorldPoint (InputManager.Instance.MousePosition);
+
+			// float offsetMagnitude = mouseOffset.magnitude;
+
+			// Vector2 thing = Camera.ScreenToWorldPoint (new Vector2 (Screen.width, Screen.height));
+			// float maxScreenVector = Mathf.Min (thing.x, thing.y);
+
+
+			// Debug.Log (offsetMagnitude + " " + maxScreenVector + " " + (maxZoom - Camera.orthographicSize));
+			// float mod = Mathf.Clamp (offsetMagnitude/maxScreenVector, 0, 1);
+			// float zoomExtendDistance = maxScreenVector - edgeHotspot*maxScreenVector;
+
+			// mod = Mathf.Clamp (zoomExtendDistance - (maxScreenVector - offsetMagnitude), 0, zoomExtendDistance)/Mathf.Abs (zoomExtendDistance);
+			// Camera.orthographicSize = Mathf.Clamp (Camera.orthographicSize + (maxZoom - Camera.orthographicSize) * mod, minZoom, maxZoom);
+
+			/* Zoom wheel zooming. */
 			float zoomDelta = InputManager.Instance.ZoomDelta;
-			zoomLerp += Time.deltaTime;
+			// if palyer is using the scroll wheel update scroll-to value and lerp time
 			if (zoomDelta != 0)
 			{
-				previousZoomValue = Camera.orthographicSize;
-				nextZoomValue = Mathf.Clamp (Camera.orthographicSize - zoomDelta*zoomMod, minZoom, maxZoom); 
 				zoomLerp = 0;
+				previousZoomValue = Camera.orthographicSize;
+				nextZoomValue = previousZoomValue + Mathf.Clamp (zoomDelta*zoomMod, minZoom - Camera.orthographicSize, maxZoom - Camera.orthographicSize);
+				// reset total time over which the lerp to the zoom value should take place
+				inverseTotalZoomTime = 1 / Mathf.Abs (singleZoomTime * zoomDelta);
 			}
-			if (zoomLerp > totalZoomLerp)
-			{
-				previousZoomValue = nextZoomValue = Camera.orthographicSize;
-			}
+			// tick for zoom lerp happens here to make the scroll more responsive (as it would begin on the same frame as player begins scrolling)
+			zoomLerp += Time.deltaTime;
 
 			// lerp the zoom into position
-			lerp = LerpFunctions.SmoothStep (zoomLerp/totalZoomLerp);
-			Camera.orthographicSize = Mathf.Lerp (previousZoomValue, nextZoomValue, lerp);
+			Camera.orthographicSize = Mathf.Lerp (previousZoomValue, nextZoomValue, zoomLerp*inverseTotalZoomTime);
 		}
 	}
 }
