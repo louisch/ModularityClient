3d84cdb12299edfdf82dc8b2d32a9097b814dae7
Louis Chan <louisch@fastmail.com>
2019-05-26 15:03:18 +0800

    Remove commented-out code

--- Assets/Scripts/Modules/Module.cs
+++ Assets/Scripts/Modules/Module.cs
@@ -3,39 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class Module : MonoBehaviour
-{
-
-	// public:
-	// list of occupied blocks
-	// list of attachment points (think GUI)
-
-	// private
-	// list of joints (in same order as joint positions)
-	//
-
-
-	//public bool[] validJoints = new bool[4]; // Values should be set in Unity editor
-	//public IntVector2[] stuff;
-	//Module[] joints = new Module[4];
+public class Module : MonoBehaviour {
 
 	public IntVector2[] occupiedBlocks;
 	public BlockInfo[] occupiedBlockJoints;
 
-	void Start ()
-	{
+	void Start() {}
 
+	public void ChangeParent(ModuleGroup parent, IntVector2 pos, int rot) {
+		SetPosition(pos);
+		SetRotation(rot);
+		transform.SetParent(parent.gameObject.transform);
 	}
 
-	public void ChangeParent (ModuleGroup parent, IntVector2 pos, int rot)
-	{
-		SetPosition (pos);
-		SetRotation (rot);
-		transform.SetParent (parent.gameObject.transform);
-	}
-
-	public void SetPosition (IntVector2 pos)
-	{ // MAGIC NUMBER HAXX (implicit x1 normalisation because blocks are currently exactly 1x1 units in size)
+	public void SetPosition (IntVector2 pos) {
+		// MAGIC NUMBER HAXX (implicit x1 normalisation because blocks are currently exactly 1x1 units in size)
 		transform.localPosition.Set (
 			Convert.ToSingle (pos.x),
 			Convert.ToSingle (pos.y),
@@ -43,43 +25,22 @@ public class Module : MonoBehaviour
 		);
 	}
 
-	public IntVector2 GetPosition ()
-	{
-		return new IntVector2 (
-			Convert.ToInt32 (transform.position.x),
-			Convert.ToInt32 (transform.position.y)
+	public IntVector2 GetPosition() {
+		return new IntVector2(
+			Convert.ToInt32(transform.position.x),
+			Convert.ToInt32(transform.position.y)
 		);
 	}
 
-	public void SetRotation (int rot)
-	{
-		transform.localEulerAngles.Set (0, 0, Convert.ToSingle (rot));
+	public void SetRotation(int rot) {
+		transform.localEulerAngles.Set(0, 0, Convert.ToSingle(rot));
 	}
 
-	public int GetRotation ()
-	{
-		return (Convert.ToInt32 (transform.localEulerAngles.z) / 90) % 4;
+	public int GetRotation() {
+		return (Convert.ToInt32(transform.localEulerAngles.z) / 90) % 4;
 	}
 
-	public Module[] GetJoints ()
-	{
+	public Module[] GetJoints() {
 		return null;
 	}
-
-	//public IntVector2 GetGlobalNeighbour ()
-	//{
-	//	IntVector2 result = GetPosition ();
-	//	switch (GetRotation ())
-	//	{
-	//		case 0:
-	//			result.y
-	//	}
-	//}
-
-
-	//void Update ()
-	//{
-	//	transform.localEulerAngles = new Vector3 (0, 0, transform.localEulerAngles.z + 1);
-	//	Debug.Log (transform.localEulerAngles.ToString ());
-	//}
 }
--- Assets/Scripts/Modules/ModuleGroup.cs
+++ Assets/Scripts/Modules/ModuleGroup.cs
@@ -2,49 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ModuleGroup : MonoBehaviour
-{
-	Dictionary<IntVector2, Module> OccupiedBlocks = new Dictionary<IntVector2, Module> ();
-	Dictionary<IntVector2, Module> APs = new Dictionary<IntVector2, Module> ();
+public class ModuleGroup : MonoBehaviour {
 
-	// bool IsValidAttachment (Module module, IntVector2 groupPosition, IntVector2 );
+	Dictionary<IntVector2, Module> OccupiedBlocks = new Dictionary<IntVector2, Module>();
+	Dictionary<IntVector2, Module> APs = new Dictionary<IntVector2, Module>();
 
-	void Start ()
-	{
+	void Start() {
 		// add all children to OccupiedBlocks
-		foreach (Transform child in transform)
-		{
-			Module childMI = child.gameObject.GetComponent<Module> ();
-			OccupiedBlocks.Add (childMI.GetPosition (), childMI);
+		foreach (Transform child in transform) {
+			Module childMI = child.gameObject.GetComponent<Module>();
+			OccupiedBlocks.Add(childMI.GetPosition(), childMI);
 		}
 
 		// generate APs - iterate through all children, generate AP if joint is free
-		foreach (KeyValuePair<IntVector2, Module> value in APs)
-		{
+		foreach (KeyValuePair<IntVector2, Module> value in APs) {
 			Module module = value.Value;
-			Module[] joints = module.GetJoints ();
-			for (int i = 0; i < joints.Length; i++)
-			{
-				//if (module.validJoints[i])
-				//{
-
-				//}
+			Module[] joints = module.GetJoints();
+			for (int i = 0; i < joints.Length; i++) {
 			}
 		}
-		Debug.Log (OccupiedBlocks.Count);
+		Debug.Log(OccupiedBlocks.Count);
 	}
 
-	public void showAPs ()
-	{
-		foreach (KeyValuePair<IntVector2, Module> value in APs)
-		{
-
+	public void showAPs() {
+		foreach (KeyValuePair<IntVector2, Module> value in APs) {
 		}
 
-		foreach (Transform child in transform)
-		{
-			//child.gameObject.SetActive (false);
+		foreach (Transform child in transform) {
 		}
-
 	}
 }
