1692f548ea6671a3de0a171ac7dac12f57dd2e7f
Louis Chan <louisch@fastmail.com>
2019-05-27 21:54:58 +0800

    Add ImpulseModule

---
+++ Assets/Scripts/Modules/ImpulseModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ImpulseModule : MonoBehaviour {
+
+    [SerializeField]
+    private float magnitudeOfForce = 3.0f;
+
+    private Rigidbody2D c_RigidBody;
+
+    // Start is called before the first frame update
+    void Start() {
+        c_RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public void ApplyImpulse() {
+        c_RigidBody.AddForce(magnitudeOfForce * transform.right, ForceMode2D.Impulse);
+    }
+}
